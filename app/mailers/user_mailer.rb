6383924f83aeb61f916e79dd78e140296316b3a9
Brian Dorton <dorton@gmail.com>
2016-04-19 09:44:14 -0500

    attaches csv to email.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,6 +2,12 @@ class UserMailer < ApplicationMailer
   def admin_email(user)
     @user = user
     @events = Event.where("date = ?", Date.today)
+
+    @events.each do |event|
+      users = event.users.order(:interest)
+      attachments["#{event.name.parameterize}.csv"] = {data: Base64.encode64(users.to_csv), encoding: 'base64' }
+    end
+
     mail(to: @user.email, subject: "TIY Houston Event Attendees")
   end
 end
