758f129701b511a57750b27623cb79774933022c
Brian Dorton <dorton@gmail.com>
2016-06-10 22:09:28 -0500

    changes interest to pie chart, reorders dates, formats dates

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -23,17 +23,17 @@ class DashboardController < ApplicationController
 
     @demo_day_goers = User.joins(:user_events => :event).where("events.name LIKE ?", "%Demo Day%").count
 
-    @interests = User.pluck('DISTINCT interest')
+    @interests = User.pluck('DISTINCT interest').reject(&:blank?)
 
     @event_names = Event.pluck('DISTINCT name')
 
-    @demo_days = Event.where(name: "Demo Day")
+    @demo_days = Event.where(name: "Demo Day").order(date: :asc)
 
-    @rails_crash_courses = Event.where(name: "Rails Crash Course")
+    @rails_crash_courses = Event.where(name: "Rails Crash Course").order(date: :asc)
 
-    @js_crash_courses = Event.where(name: "JS Crash Course")
+    @js_crash_courses = Event.where(name: "JS Crash Course").order(date: :asc)
 
-    @ios_crash_courses = Event.where(name: "iOS Crash Course")
+    @ios_crash_courses = Event.where(name: "iOS Crash Course").order(date: :asc)
 
     @students = User.where(student: true)
 
